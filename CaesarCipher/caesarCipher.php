2948cba2ea17d7b6497593da7365bfaab2564ee6
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-03-26 17:09:50 -0300

    Adiciona algoritmo de cifra de cesar

---
+++ CaesarCipher/caesarCipher.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+echo "Enter the phrase: ";
+$phrase = readline();
+
+echo "Enter the k: ";
+$k = readline();
+
+$newPhrase = "";
+$phraseLenght = strlen($phrase);
+$lettersFrequency = [];
+for($count = 0; $count < $phraseLenght; $count++) {
+    for($countK = 0; $countK < $k; $countK++) {
+        $letter = $phrase[$count];
+        $newPhrase[$count] = (strlen(++$letter) > 0) ? $letter[0] : $letter;
+    }
+    if(!isset($lettersFrequency[$phrase[$count]])) {
+        $lettersFrequency[$phrase[$count]] = 1;
+        continue;
+    }
+    $lettersFrequency[$phrase[$count]]++;
+}
+
+foreach($lettersFrequency as $key => $letterFrequency) {
+    $lettersFrequency[$key] = $letterFrequency / $phraseLenght;
+}
+
+var_dump($);
+
+
